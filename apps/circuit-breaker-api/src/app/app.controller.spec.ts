6cdd384116581d3a83c0cddfa7c9cac61525a3c5
Preston Lamb <preston.j.lamb@gmail.com>
2026-02-13 20:37:55 -0700

    test: fix unit tests for auth features and api

--- apps/circuit-breaker-api/src/app/app.controller.spec.ts
+++ apps/circuit-breaker-api/src/app/app.controller.spec.ts
@@ -1,21 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from '@circuit-breaker/api/feature/auth';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let appController: AppController;
+  let authService: AuthService;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
+  const mockAuthService = {
+    login: jest.fn(),
+  };
+
+  const mockAppService = {
+    getData: jest.fn().mockReturnValue({ message: 'Hello API' }),
+  };
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        { provide: AppService, useValue: mockAppService },
+        { provide: AuthService, useValue: mockAuthService },
+      ],
     }).compile();
+
+    appController = app.get<AppController>(AppController);
+    authService = app.get<AuthService>(AuthService);
   });
 
   describe('getData', () => {
     it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
       expect(appController.getData()).toEqual({ message: 'Hello API' });
+    });
+  });
+
+  describe('login', () => {
+    it('should return access token', async () => {
+      const req = { user: { username: 'test' } };
+      const expectedResult = { access_token: 'token' };
+      mockAuthService.login.mockResolvedValue(expectedResult);
+
+      expect(await appController.login(req)).toEqual(expectedResult);
+      expect(mockAuthService.login).toHaveBeenCalledWith(req.user);
+    });
+  });
+
+  describe('getProfile', () => {
+    it('should return user from request', () => {
+      const req = { user: { username: 'test' } };
+      expect(appController.getProfile(req)).toEqual(req.user);
     });
   });
 });
